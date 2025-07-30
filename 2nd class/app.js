aac79b782436b483c03a2de08e2405cb3297a68c
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-07-30 16:55:13 +0500

    Vote Eligibility System 🗳️🧑‍⚖️

--- 2nd class/app.js
+++ 2nd class/app.js
@@ -24,6 +24,20 @@ console.log("<br>"+"<br>"+nmb)
 console.log("<br>"+"Rs"+parseFloat(nmb))
 
 
-nmb = "80.6574kg"
-document.write("<br>"+"<br>"+nmb)
-document.write("<br>"+parseInt(nmb)+" KG")
+// nmb = "80.6574kg"
+// document.write("<br>"+"<br>"+nmb)
+// document.write("<br>"+parseInt(nmb)+" KG")
+  
+
+let age = prompt("pleas Enter Your age");
+age = parseInt(age);
+if(age >= 18 ){
+    document.write("You are eligible to vote")
+}
+if(age <= 18 ){
+    document.write("You are Not  eligible to vote")
+}
+age = isNaN(age)
+if(age == true){
+    document.write("Pleas Enter Vaild numver")
+}
