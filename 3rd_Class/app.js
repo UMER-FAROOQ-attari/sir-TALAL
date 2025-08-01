0e121456cf880cd11082eb6e702de63d8c2249e8
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-01 17:06:46 +0500

    3rd class game

---
+++ 3rd_Class/app.js
@@ -0,0 +1,15 @@
+let User_amount = document.getElementById("amount");
+let masg = document.getElementById("masg");
+let bit_amount ;
+let bit_number
+function playgame(){
+    bit_amount = +("pleas ennter your bit Ammount");
+    bit_number = +("plea enter Your bit Number")
+    let makebit = Math.ceil(Math.random()*10);
+    let matcted = matcted()
+}
+
+function matcted(){
+    let T_matched = false;
+    if(bit)
+}
