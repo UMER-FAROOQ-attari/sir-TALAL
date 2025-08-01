549fa993aca1b2099ab1ee8d7ea48bcd9a0b1f5e
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-01 17:23:56 +0500

    login

--- 3rd_Class/app.js
+++ 3rd_Class/app.js
@@ -1,15 +1,34 @@
-let User_amount = document.getElementById("amount");
-let masg = document.getElementById("masg");
-let bit_amount ;
-let bit_number
-function playgame(){
-    bit_amount = +("pleas ennter your bit Ammount");
-    bit_number = +("plea enter Your bit Number")
-    let makebit = Math.ceil(Math.random()*10);
-    let matcted = matcted()
-}
+// let User_amount = document.getElementById("amount");
+// let masg = document.getElementById("masg");
+// let bit_amount ;
+// let bit_number
+// function playgame(){
+//     bit_amount = +("pleas ennter your bit Ammount");
+//     bit_number = +("plea enter Your bit Number")
+//     let makebit = Math.ceil(Math.random()*10);
+//     let matcted = matcted(makebit,bit_number)
+//     show_masg(matcted,bit_amount)
+// }
 
-function matcted(){
-    let T_matched = false;
-    if(bit)
+// function matcted(makebit , bit_number,){
+//     let T_matched = false;
+//     if(makebit == bit_number){
+//         T_matched =true;
+//     }
+//     return (T_matched)
+// }
+// function show_masg(matcted,bit_amount){
+//     if(matcted){
+//         User_amount.innerHTML = Number(User_amount.innerHTML) +
+//     }
+
+// }
+function playgame(){
+let user_email = document.getElementById("email")
+let password = document.getElementById("password")
+console.log(user_email.value,password.value)
+
+}
+function login(){
+    
 }
