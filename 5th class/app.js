db151575c0f6601a2078454e50ff87ce4984115c
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-08 16:57:53 +0500

    stop interval

--- 5th class/app.js
+++ 5th class/app.js
@@ -4,6 +4,7 @@
 // let augest = aguest_14.getTime();
 // let days = ( augest -  today ) / 1000 / 60 / 60 /24;
 // document.write(Math.ceil(days) )
+function dt (){
 let hours = document.getElementById("hours")
 let mints = document.getElementById("mints")
 let sec = document.getElementById("sec")
@@ -14,4 +15,27 @@ let s = date.getSeconds();
 hours.innerHTML  =  h;
 mints.innerHTML = m;
 sec.innerHTML = s;    
-    
+if(h>12){
+hours.innerHTML  = (h-12);
+if(h>10){
+hours.innerHTML  = "0"+(h-12);
+}
+}
+if(m<10){
+mints.innerHTML  = "0"+m;
+}
+if(s<10){
+sec.innerHTML  = "0"+s;
+}}
+
+setInterval(dt , 1000)
+
+
+let i = 0;
+function print_name(){
+    if(i>=10){
+        clearInterval(myint)
+    }
+    console.log(i++)
+}
+let myint = setInterval(print_name, 100)
