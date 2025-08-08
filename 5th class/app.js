57910f31be13fbfb3fac8701868541704f1cba2f
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-08 16:31:11 +0500

    days left in 14 augest

---
+++ 5th class/app.js
@@ -0,0 +1,6 @@
+// let rightnow = new Date();
+// let aguest_14 = new Date("14 August , 2025");
+// let today = rightnow.getTime()
+// let augest = aguest_14.getTime();
+// let days = ( augest -  today ) / 1000 / 60 / 60 /24;
+// document.write(Math.ceil(days) )
