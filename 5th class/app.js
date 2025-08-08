8e4cde8e899de1cae36e2f7b491520189cecb203
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-08 16:37:37 +0500

    right time

--- 5th class/app.js
+++ 5th class/app.js
@@ -4,3 +4,14 @@
 // let augest = aguest_14.getTime();
 // let days = ( augest -  today ) / 1000 / 60 / 60 /24;
 // document.write(Math.ceil(days) )
+let hours = document.getElementById("hours")
+let mints = document.getElementById("mints")
+let sec = document.getElementById("sec")
+let date = new Date();
+let h = date.getHours();
+let m = date.getMinutes();
+let s = date.getSeconds();   
+hours.innerHTML  =  h;
+mints.innerHTML = m;
+sec.innerHTML = s;    
+    
