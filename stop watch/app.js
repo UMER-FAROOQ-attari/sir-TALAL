7d8579ccc71b49d048546add0eff67247eb91a57
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-10 20:47:05 +0500

    mint update

--- stop watch/app.js
+++ stop watch/app.js
@@ -1,9 +1,15 @@
 let hours = document.getElementById("hours");
 let mints = document.getElementById("mints");
 let sec = document.getElementById("sec");
+let mint = 0;
 let second = 0;
 function stpwatch (){
   second++;
   sec.innerHTML = second
+  mints.innerHTML = mint;
+  if(second == 60){
+    second = 0;
+    mint++;
+  }
 }
-setInterval(stpwatch , 1000)
+setInterval(stpwatch , 100)
