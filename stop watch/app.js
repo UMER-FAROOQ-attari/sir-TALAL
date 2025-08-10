bc44017ffe31eaab9f14ad78b1b374f87c43a8fc
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-10 20:41:27 +0500

    a problum in code

--- stop watch/app.js
+++ stop watch/app.js
@@ -1,3 +1,11 @@
 let hours = document.getElementById("hours");
 let mints = document.getElementById("mints");
 let sec = document.getElementById("sec");
+function stpwatch (){
+  let seconed = 0;
+    if(seconed <= 60 ){
+        seconed++
+        sec.innerHTML = seconed;
+    } 
+}
+setInterval(stpwatch , 1000)
