5ed612281bd28dd55d3e0d12427f9eaf823a93be
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-10 20:49:02 +0500

    only hour mint sec comple

--- stop watch/app.js
+++ stop watch/app.js
@@ -3,13 +3,19 @@ let mints = document.getElementById("mints");
 let sec = document.getElementById("sec");
 let mint = 0;
 let second = 0;
+let hur = 0;
 function stpwatch (){
   second++;
   sec.innerHTML = second
   mints.innerHTML = mint;
+  hours.innerHTML = hur;
   if(second == 60){
     second = 0;
     mint++;
   }
+    if(mint == 60 ){
+        mint = 0;
+        hur++;
+    }
 }
-setInterval(stpwatch , 100)
+setInterval(stpwatch , 1)
