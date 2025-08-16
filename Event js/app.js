9b70aab3441d9ee7d843eceb9a9b5e7ecbb81247
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-16 18:46:14 +0500

    Mouse Events

--- Event js/app.js
+++ Event js/app.js
@@ -3,3 +3,8 @@ function func (){
     h1.style.backgroundColor = "yellow"
     h1.style.color = "black"
 }
+// `mousedown` triggers when the mouse button is pressed down.
+// `click` triggers when the mouse button is pressed and released.
+// &&
+// mousemove: The event will fire every time the mouse moves.
+// mouseover: The event will fire when the mouse first moves over the element.
