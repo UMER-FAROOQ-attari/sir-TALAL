6ac0e07876f4422383836b78bfa086ce71a43e8a
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-08-19 16:52:28 +0500

    complete

--- Event js/image change/app.js
+++ Event js/image change/app.js
@@ -1,1 +1,41 @@
-let img = document.getElementById
+let img = document.getElementById("imge")
+function change(){
+let sorc = img.getAttribute('src')
+
+    if ( sorc == "1.jpg") {
+        img.src = "2.jpg";
+    }
+     if (sorc === "2.jpg") {
+        img.src = "3.jpg";
+    }
+    if (sorc === "3.jpg") {
+        img.src = "4.jpg";
+    }
+    
+    if (sorc === "4.jpg") {
+        img.src = "5.jpg";
+    }
+    
+    if (sorc === "5.jpg") {
+        img.src = "6.jpg";
+    }
+    
+    if (sorc === "6.jpg") {
+        img.src = "7.jpg";
+    }
+    
+    if (sorc === "7.jpg") {
+        img.src = "8.jpg";
+    }
+    
+    if (sorc === "8.jpg") {
+        img.src = "9.jpg";
+    }
+    
+    if (sorc === "9.jpg") {
+        img.src = "10.jpg";
+    }
+    if (sorc === "10.jpg") {
+        img.src = "1.jpg";
+    }
+}
