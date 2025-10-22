9cb823eb650f6a7405798d4cce42faa90e8250a9
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-22 16:25:48 +0500

    first letter upper logic

---
+++ 15th/app.js
@@ -0,0 +1,9 @@
+let names= ["ali","abuBaker","TAlAl","MoiZ"];
+let fullName = "";
+for(let i=0;i<names.length;i++){
+    let name = names[i];
+    let firstletter = name.charAt(0).toLocaleUpperCase();
+    let rest = name.slice(1).toLowerCase()
+    fullName += firstletter + rest + " "
+}
+console.log(fullName)
