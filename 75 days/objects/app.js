2a5bc88c2ed21f01b2d6a72f3e9bc2ee8019e934
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-23 15:57:47 +0500

    object practice

--- 75 days/objects/app.js
+++ 75 days/objects/app.js
@@ -1,10 +1,33 @@
-let object = {
+// let object = {
 
-}
-object.name = "John"
-object.surname = "Smith"
-console.log(object)
-object.name = "Pete"
-console.log(object)
-delete object.name
-console.log(object)
+// }
+// object.name = "John"
+// object.surname = "Smith"
+// console.log(object)
+// object.name = "Pete"
+// console.log(object)
+// delete object.name
+// console.log(object)
+
+// let objects ={
+//     name:"umer",
+//     fathername:"akbar ali",
+//     fun (){
+//         alert(`your name is ${this.name} and your father name is ${this.fathername}`)
+//     }
+// }
+// objects.fun()
+// let coopy = objects;
+// objects.name = "abubaker";
+// objects.fun()
+    
+// function registeration (name){
+// this.name = name;
+// this.isAdmin = false
+// }
+// let user1 = new registeration("umer");
+// let user2 = new registeration("bakker");
+// alert(user1.name)
+// alert(user2.name)
+let user = {};
+alert(user?.name?.street)
