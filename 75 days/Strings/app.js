e131049e0385da7f00b34532e4800deffcf9b591
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-23 17:44:19 +0500

    today compelete practice

--- 75 days/Strings/app.js
+++ 75 days/Strings/app.js
@@ -2,4 +2,15 @@
 //   John
 //   Pete`;
 //  alert(guestList)
- alert( 'I \t the Walrus!' ); // tab space
+//  alert( 'I \t the Walrus!' ); // tab space
+// let ame = "umer";
+// alert(ame.at(-1));
+// let str = " umer "
+// alert(str.trim())
+let search = prompt("Pleas search here");
+search = search.toLocaleLowerCase();
+if(search.includes("xxx")||search.includes("fuck")){
+    alert("pleas right word search")
+}
+
+// alert("umerfarooq".localeCompare("abubakker"))
