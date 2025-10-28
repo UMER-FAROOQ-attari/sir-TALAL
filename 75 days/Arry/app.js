b23698ba4b163bbd3927f4fa1649779d9fdb83cd
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-28 15:37:15 +0500

    compelete arry

--- 75 days/Arry/app.js
+++ 75 days/Arry/app.js
@@ -3,3 +3,22 @@ console.log(arrys);
 arrys[0] = "Umar"
 console.log(arrys);
 console.log(arrys.length);
+console.log(arrys.at(-1));
+arrys.pop();
+arrys.shift()
+arrys.push("Farooq");
+arrys.unshift("Siddique");
+console.log(arrys)
+for (let arry of arrys ){
+    console.log(arry);
+}
+console.log(String(arrys))
+let arryofNo =[1,2,3,4,5];
+let sum = arryofNo.reduce((choti , bari)=> choti+bari,0);
+console.log(sum);
+let dubale = arryofNo.map(n=>n*2);
+console.log(dubale);
+let sum2 = dubale.reduce((choti , bari)=> choti+bari,0);
+console.log(sum2);
+let joins = arryofNo.join("0")
+console.log(joins)
