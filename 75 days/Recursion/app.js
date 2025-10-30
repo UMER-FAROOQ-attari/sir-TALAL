70671be103dc2e76e6efd9b67c5cb970325604c6
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-30 16:05:50 +0500

    Recrison is complete

--- 75 days/Recursion/app.js
+++ 75 days/Recursion/app.js
@@ -1,6 +1,27 @@
-function chal(){
-console.log("i am umer")
-    chal()
+// function chal(){
+// console.log("i am umer")
+//     chal()
+    
+// }
+// chal()
+
+function coding(num){
+if(num === 0){
+    console.log("codeing is complete ✔")
+}
+else{
+    console.log("codeing is contiue 📄📄");
+    coding(num-1)
     
 }
-chal()
+}
+coding(5)
+function factorial(n){
+    if(n === 1){
+        return 1
+    }
+    let fact = n * factorial(n-1)
+    return fact
+}
+console.log(factorial(1))
+
