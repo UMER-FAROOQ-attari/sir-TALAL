bd0befead8d849c1fbe74236f5da23a5d4b66ff8
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-01 23:34:34 +0500

    yes

--- assigment/app.js
+++ assigment/app.js
@@ -1,14 +1,25 @@
-let searchbar = document.getElementById("searchbar");
-let li = document.querySelectorAll("li")
-let ul  = document.querySelector("ul")
-function answer(){
-        let serachWorld = searchbar.value.toLowerCase()
-            ul.innerHTML = ""
-
-    li.forEach(city=>{  
-        let citye = city.innerHTML.toLowerCase()
-        if(citye.includes(serachWorld)){
-            ul.innerHTML += `<li>${citye}</li>`
-        }
-    })
+let city = document.getElementById("city")
+let province = document.getElementById("province")
+function applyStyle(){
+          city.style.backgroundColor = "black";
+            city.style.color = "white";
+            city.style.borderColor = "white";  
+            city.style.fontWeight = "bold";
 }
+function getpro(){
+     let text = city.value;
+    let firstLetter  = text.charAt(0).toUpperCase()
+    let restLetter = text.slice(1).toLowerCase()
+    city.value = firstLetter + restLetter;
+    if(city.value == "Lahore"){
+        province.value = "Panjab"
+    }if(city.value == "Karachi"){
+        province.value = "Sindh"
+    }if(city.value == "Peshawar"){
+        province.value = "Kpk"
+    }if(city.value == "Quetta"){
+        province.value = "Balochistan"
+    }if(city.value == "Islamabad"){
+        province.value = "Pakistan"
+    }
+    }
