9c1f2a1a38db9e6f841a3514df82a4ecaf2bbc9d
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-03 17:11:32 +0500

    Error

--- do while/app.js
+++ do while/app.js
@@ -3,9 +3,14 @@
 //     console.log(`Number  ${i}`)
 //     i++
 // }
+// let password ;
+// do{
+// password = +prompt("Enter Your password ....")
+// }
+// while(password !== 1234)
+// console.log("yess YOUR are L0G1N");
 let password ;
-do{
-password = +prompt("Enter Your password ....")
+while (password !== "1234"){
+    password = +prompt("Enter Your password ....")
+    console.log("yes")
 }
-while(password !== 1234)
-console.log("yess YOUR are L0G1N")
