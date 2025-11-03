0c7c3aae72e4e7df5bd9e1eeb61085b391c04e72
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-03 17:33:39 +0500

    light off

--- do while/app.js
+++ do while/app.js
@@ -3,14 +3,28 @@
 //     console.log(`Number  ${i}`)
 //     i++
 // }
-// let password ;
 // do{
 // password = +prompt("Enter Your password ....")
 // }
 // while(password !== 1234)
 // console.log("yess YOUR are L0G1N");
-let password ;
-while (password !== "1234"){
-    password = +prompt("Enter Your password ....")
-    console.log("yes")
+// let password;
+// while (password !== "1234") {
+//     password = prompt("Enter Your password ....")
+// }
+// console.log("yes")
+let guess = Math.ceil(Math.random() * 3) //7
+let userGuess;
+do {
+    userGuess = +prompt("enter your number?")
+    if (userGuess === guess) {
+        alert("you Guessed right!")
+        console.log(`your number is ${guess}`)
+    }
+    else if(userGuess < guess) {
+        alert("t00 low");
+    } else {
+        alert("t00 high  ")
+    }
 }
+while (userGuess !== guess);
