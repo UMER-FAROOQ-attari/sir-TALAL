cd96809d478523d8242061875b53756bf8cbfc2b
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-03 17:06:41 +0500

    do while

---
+++ do while/app.js
@@ -0,0 +1,11 @@
+// let i = 0;
+// while(i<5){
+//     console.log(`Number  ${i}`)
+//     i++
+// }
+let password ;
+do{
+password = +prompt("Enter Your password ....")
+}
+while(password !== 1234)
+console.log("yess YOUR are L0G1N")
