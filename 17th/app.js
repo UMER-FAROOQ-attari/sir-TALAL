4d5d7b9fe73fd53c5afffb07937ed911e9e91bda
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-06 16:36:24 +0500

    Destructuring old way

--- 17th/app.js
+++ 17th/app.js
@@ -2,7 +2,17 @@
 // for(let Name of arr){
 //     console.log(Name)
 // }
-let arr = ["Abubaker","Umer","Usaman","Ali"];
-for(let numb in arr){
-    console.log(numb)
-}
+// let arr = ["Abubaker","Umer","Usaman","Ali"];
+// for(let numb in arr){
+//     console.log(numb)
+// }
+const fruits = ["Apple", "Orange", "Banana", "Mango"];
+let fruit1 = fruits[0];
+let fruit2 = fruits[1];
+let fruit3 = fruits[2]
+let fruit4 = fruits[3]
+console.log(fruits)
+console.log(fruit1)
+console.log(fruit2)
+console.log(fruit3)
+console.log(fruit4)
