b1f385fce2a62881b8218f65602d8e15676c3ca1
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-06 16:37:50 +0500

    new style

--- 17th/app.js
+++ 17th/app.js
@@ -6,11 +6,18 @@
 // for(let numb in arr){
 //     console.log(numb)
 // }
+// const fruits = ["Apple", "Orange", "Banana", "Mango"];
+// let fruit1 = fruits[0];
+// let fruit2 = fruits[1];
+// let fruit3 = fruits[2]
+// let fruit4 = fruits[3]
+// console.log(fruits)
+// console.log(fruit1)
+// console.log(fruit2)
+// console.log(fruit3)
+// console.log(fruit4)
 const fruits = ["Apple", "Orange", "Banana", "Mango"];
-let fruit1 = fruits[0];
-let fruit2 = fruits[1];
-let fruit3 = fruits[2]
-let fruit4 = fruits[3]
+let [fruit1 , fruit2 ,fruit3,fruit4] = fruits;
 console.log(fruits)
 console.log(fruit1)
 console.log(fruit2)
