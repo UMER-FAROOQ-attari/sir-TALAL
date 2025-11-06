51472e56293658e1a81f2edb64967e47c387b3c7
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-06 16:32:08 +0500

    for…in loop
    defrance between for…in loop & for…of loop

--- 17th/app.js
+++ 17th/app.js
@@ -1,4 +1,8 @@
+// let arr = ["Abubaker","Umer","Usaman","Ali"];
+// for(let Name of arr){
+//     console.log(Name)
+// }
 let arr = ["Abubaker","Umer","Usaman","Ali"];
-for(let Name of arr){
-    console.log(Name)
+for(let numb in arr){
+    console.log(numb)
 }
