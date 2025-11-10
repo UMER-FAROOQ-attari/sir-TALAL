6243eddef8f3b6ed3324d127b2af368acb028695
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-10 17:34:22 +0500

    Map

--- ES6/app.js
+++ ES6/app.js
@@ -1,8 +1,17 @@
-let arr = new Set(["Talal","Ahmed","Talal","ashraf","haider sha","Nerooz","Abdullah"]);
-console.log(arr.has("Ahmed"));
-console.log(arr.add("Umer"));
-console.log(arr.delete("haider sha"));
-console.log(arr)
-console.log(arr.size);
-let realArr = [...arr];
-console.log(realArr)
+// let arr = new Set(["Talal","Ahmed","Talal","ashraf","haider sha","Nerooz","Abdullah"]);
+// console.log(arr.has("Ahmed"));
+// console.log(arr.add("Umer"));
+// console.log(arr.delete("haider sha"));
+// console.log(arr)
+// console.log(arr.size);
+// let realArr = [...arr];
+// console.log(realArr)
+
+let obj = new Map([
+    ["fullname","UMER FAROOQ"],
+    ["age",19],
+    ["web devloper",true]
+])
+obj.clear()
+console.log(obj)
+console.log()
