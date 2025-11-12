1f31075170dfe38d60ab0eab13e72dcfac5c1b89
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-12 17:13:45 +0500

    Count Down

--- 18th lab/app.js
+++ 18th lab/app.js
@@ -1,15 +1,23 @@
-let arr = [255,67,78,45];
-let sub = 0;
-for(let numb of arr){
-  sub += numb
-}
-console.log(sub)
+// let arr = [255,67,78,45];
+// let sub = 0;
+// for(let numb of arr){
+//   sub += numb
+// }
+// console.log(sub)
 
-let string = "TALAL" ;
-let str = [];
-for(let letter of string){
-   str.unshift(letter)
-     
+// let string = "TALAL" ;
+// let str = [];
+// for(let letter of string){
+//    str.unshift(letter)
+
+// }
+// str = str.join("")
+// console.log(str)
+
+let count = 5;
+do {
+    console.log(`Game is starting in  ${count} Sec`);
+    count--;
 }
-str = str.join("")
-console.log(str)
+while (count != 0)
+console.log(count)
