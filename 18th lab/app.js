837d8e4bdaaf0bc0df4480bae49ee0c5ab169e31
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-12 16:42:52 +0500

    for in & for of loop

---
+++ 18th lab/app.js
@@ -0,0 +1,15 @@
+let arr = [255,67,78,45];
+let sub = 0;
+for(let numb of arr){
+  sub += numb
+}
+console.log(sub)
+
+let string = "TALAL" ;
+let str = [];
+for(let letter of string){
+   str.unshift(letter)
+     
+}
+str = str.join("")
+console.log(str)
