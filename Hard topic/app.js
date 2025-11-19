34bcd02d5052a72d8d83e61c862bdeab9c85d019
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-19 17:17:03 +0500

    try catch

--- Hard topic/app.js
+++ Hard topic/app.js
@@ -1,11 +1,23 @@
-/* Encapsulation  */
-class Car{
-     start(){
-        this.#engineStartded()
-     }
-    #engineStartded(){
-        console.log("Enigine is Started . . . . .")
-    }
+// /* Encapsulation  */
+// class Car{
+//      start(){
+//         this.#engineStartded()
+//      }
+//     #engineStartded(){
+//         console.log("Enigine is Started . . . . .")
+//     }
+// }
+// let care1 = new Car()
+// care1.start()
+console.log("1")
+//  undefinedFunction();
+//  console.log("2")
+try {
+console.log("2")
+
+   undefinedFunction();
 }
-let care1 = new Car()
-care1.start()
+catch (error){ 
+       console.error("Eror Function not define! ",error)
+}
+console.log("3")
