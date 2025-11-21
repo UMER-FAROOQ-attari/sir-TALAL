7ed095ac4be51630f5f9769b961061ed20e9d2d9
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-21 17:25:36 +0500

    password

--- Hard topic/practice/app.js
+++ Hard topic/practice/app.js
@@ -1,13 +1,27 @@
-class BankAccount{
-    #blance = 0;
-    deposit(amount){
-        this.#blance += amount
+// class BankAccount{
+//     #blance = 0;
+//     deposit(amount){
+//         this.#blance += amount
+//     }
+//     getBlance(){
+//         return this.#blance
+//     }
+// }
+// let umer = new BankAccount();
+// umer.deposit(500);
+// console.log(umer.getBlance())
+// // console.log(umer.#blance)
+class User{
+    #password;
+    constructor(username , password){
+        this.username = username;
+        this.#password = password
     }
-    getBlance(){
-        return this.#blance
+    checkPassword(input){
+     return this.#password === input;
     }
 }
-let umer = new BankAccount();
-umer.deposit(500);
-console.log(umer.getBlance())
-// console.log(umer.#blance)
+
+const user1 = new User("umer","12345");
+console.log(user1.username);
+console.log(user1.#pa);
