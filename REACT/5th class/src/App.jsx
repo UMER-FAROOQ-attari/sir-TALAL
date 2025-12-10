abc21c00c6f1c4459f4c5fde6ce4a4d69805109b
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-10 17:39:38 +0500

    yes hogya

--- REACT/5th class/src/App.jsx
+++ REACT/5th class/src/App.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react'
 import './App.css'
-function App() {
-
+function App({msg}) {
+let [showBadge, setShowBadge] = useState(false)
+useEffect(()=>{
+  console.log("efacteded .....")
+},[])
+const  handle = ()=>{
+setShowBadge(true)
+}
   return (
     <>
-  
-  <h1></h1>
-    </>
+  <div>
+<button className='border-2 p-2 rounded-full text-red-900 text-center  bg-pink-300 font-bold'   onClick={handle}>Login</button>
+ {showBadge &&  <h1 className="text-5xl bg-green-300 font-bold rounded-3xl shadow-lg shadow-gray-500/75 m-5 text-blue-500 p-10 text-center leading-tight">
+  {msg}
+   
+
+</h1>  }
+</div>  </>
   )
 }
 
