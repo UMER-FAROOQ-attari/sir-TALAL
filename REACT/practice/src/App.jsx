7eff56231729fe9b005ec0ad219e55285fe08b2f
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-14 15:24:10 +0500

    no render only first time

--- REACT/practice/src/App.jsx
+++ REACT/practice/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 useEffect(()=>{
   document.title = count
   
-})
+},[])
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8 font-sans">
