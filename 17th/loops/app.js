8f1a371aabf1a8f9d92e6c86c1db57b8b27b77dc
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-21 15:43:25 +0500

    41 while loops

---
+++ 17th/loops/app.js
@@ -0,0 +1,12 @@
+// for(var i = 0; i <= 5 ; i++ ){
+//     console.log(i)
+// }
+
+// var  i = 0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+for (var i = 5; i > 0; i--) {
+  console.log(i);
+}
