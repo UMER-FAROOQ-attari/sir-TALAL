b209479e05701cf3f7cffabd1331c95b288376ff
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-21 16:04:34 +0500

    Commenting

--- 17th/loops/app.js
+++ 17th/loops/app.js
@@ -1,21 +1,28 @@
-// // for(var i = 0; i <= 5 ; i++ ){
-// //     console.log(i)
-// // }
+// // // for(var i = 0; i <= 5 ; i++ ){
+// // //     console.log(i)
+// // // }
 
-// // var  i = 0;
-// // while(i<5){
-// //     console.log(i);
-// //     i++;
+// // // var  i = 0;
+// // // while(i<5){
+// // //     console.log(i);
+// // //     i++;
+// // // }
+// // for (var i = 5; i > 0; i--) {
+// //   console.log(i);
 // // }
-// for (var i = 5; i > 0; i--) {
+// var i = 0;
+// do{
+//     alert(i);
+//     i++;
+// }while(i<3)
+//     var i = 0;
+// while (i < 0) {
 //   console.log(i);
 // }
-var i = 0;
-do{
-    alert(i);
-    i++;
-}while(i<3)
-    var i = 0;
-while (i < 0) {
-  console.log(i);
-}
+/*43 Placing scripts   */ 
+var i = 0 ; // single line comment
+/* 
+many 
+line
+comment  */
+// comment just for readalbe and debuging
