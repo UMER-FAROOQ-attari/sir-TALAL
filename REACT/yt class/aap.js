3f360c726a8cfcc551e81342b3754417c84b7f33
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-29 10:22:47 +0500

    1 hr complete

--- REACT/yt class/aap.js
+++ REACT/yt class/aap.js
@@ -2,4 +2,5 @@ import naam from './test.js'
 import {age} from './test.js'
 let head =  document.createElement('h1');
 head.innerHTML = (`mera name ${naam} or meri age ${age} Year Old hy`)
-document.body.appendChild(head)
+document.body.appendChild(head);
+document.getElementById("head2").render(age)
