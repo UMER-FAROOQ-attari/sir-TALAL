d4d8fcb05c770da211024a2e1bdb31794d569604
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-30 15:19:51 +0500

    scroling

---
+++ REACT/yt class/react 2nd with tailwind css/src/App.jsx
@@ -0,0 +1,23 @@
+const App = () => {
+  return (
+    // 1. Parent: flex + overflow-x-auto
+    <div className="flex overflow-x-auto gap-4 p-5 bg-gray-100">
+      
+      {/* 2. Children: Fixed Width + shrink-0 */}
+      <div className="w-40 h-40 bg-red-500 shrink-0 rounded-lg">Box 1</div>
+      <div className="w-40 h-40 bg-blue-500 shrink-0 rounded-lg">Box 2</div>
+      <div className="w-40 h-40 bg-green-500 shrink-0 rounded-lg">Box 3</div>
+      <div className="w-40 h-40 bg-yellow-500 shrink-0 rounded-lg">Box 4</div>
+      <div className="w-40 h-40 bg-purple-500 shrink-0 rounded-lg">Box 5</div>
+      <div className="w-40 h-40 bg-pink-500 shrink-0 rounded-lg">Box 6</div>
+      <div className="w-40 h-40 bg-red-500 shrink-0 rounded-lg">Box 1</div>
+      <div className="w-40 h-40 bg-blue-500 shrink-0 rounded-lg">Box 2</div>
+      <div className="w-40 h-40 bg-green-500 shrink-0 rounded-lg">Box 3</div>
+      <div className="w-40 h-40 bg-yellow-500 shrink-0 rounded-lg">Box 4</div>
+      <div className="w-40 h-40 bg-purple-500 shrink-0 rounded-lg">Box 5</div>
+      <div className="w-40 h-40 bg-pink-500 shrink-0 rounded-lg">Box 6</div>
+    </div>
+  )
+}
+
+export default App
