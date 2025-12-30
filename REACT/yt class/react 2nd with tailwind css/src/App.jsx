719dca9e83f628838f2e289e26a05f60b875c3d2
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-12-30 17:49:16 +0500

    object change

--- REACT/yt class/react 2nd with tailwind css/src/App.jsx
+++ REACT/yt class/react 2nd with tailwind css/src/App.jsx
@@ -1,22 +1,51 @@
+import { useState } from "react"
+
 const App = () => {
+  const [user, setUser] = useState({name:"umer",age:19});
+  function add(){
+    setUser(pre=>({...pre,age:20}))
+  }
   return (
-    // 1. Parent: flex + overflow-x-auto
-    <div className="flex overflow-x-auto gap-4 p-5 bg-gray-100">
+   <>
+    <h1>He is {user.name} and age is {user.age}</h1>   
+    <button onClick={add}> Add</button>  
+   </>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // 1. Parent: flex + overflow-x-auto
+    // <div className="flex overflow-x-auto gap-4 p-5 bg-gray-100">
       
-      {/* 2. Children: Fixed Width + shrink-0 */}
-      <div className="w-40 h-40 bg-red-500 shrink-0 rounded-lg">Box 1</div>
-      <div className="w-40 h-40 bg-blue-500 shrink-0 rounded-lg">Box 2</div>
-      <div className="w-40 h-40 bg-green-500 shrink-0 rounded-lg">Box 3</div>
-      <div className="w-40 h-40 bg-yellow-500 shrink-0 rounded-lg">Box 4</div>
-      <div className="w-40 h-40 bg-purple-500 shrink-0 rounded-lg">Box 5</div>
-      <div className="w-40 h-40 bg-pink-500 shrink-0 rounded-lg">Box 6</div>
-      <div className="w-40 h-40 bg-red-500 shrink-0 rounded-lg">Box 1</div>
-      <div className="w-40 h-40 bg-blue-500 shrink-0 rounded-lg">Box 2</div>
-      <div className="w-40 h-40 bg-green-500 shrink-0 rounded-lg">Box 3</div>
-      <div className="w-40 h-40 bg-yellow-500 shrink-0 rounded-lg">Box 4</div>
-      <div className="w-40 h-40 bg-purple-500 shrink-0 rounded-lg">Box 5</div>
-      <div className="w-40 h-40 bg-pink-500 shrink-0 rounded-lg">Box 6</div>
-    </div>
+    //   {/* 2. Children: Fixed Width + shrink-0 */}
+    //   <div className="w-40 h-40 bg-red-500 shrink-0 rounded-lg">Box 1</div>
+    //   <div className="w-40 h-40 bg-blue-500 shrink-0 rounded-lg">Box 2</div>
+    //   <div className="w-40 h-40 bg-green-500 shrink-0 rounded-lg">Box 3</div>
+    //   <div className="w-40 h-40 bg-yellow-500 shrink-0 rounded-lg">Box 4</div>
+    //   <div className="w-40 h-40 bg-purple-500 shrink-0 rounded-lg">Box 5</div>
+    //   <div className="w-40 h-40 bg-pink-500 shrink-0 rounded-lg">Box 6</div>
+    //   <div className="w-40 h-40 bg-red-500 shrink-0 rounded-lg">Box 1</div>
+    //   <div className="w-40 h-40 bg-blue-500 shrink-0 rounded-lg">Box 2</div>
+    //   <div className="w-40 h-40 bg-green-500 shrink-0 rounded-lg">Box 3</div>
+    //   <div className="w-40 h-40 bg-yellow-500 shrink-0 rounded-lg">Box 4</div>
+    //   <div className="w-40 h-40 bg-purple-500 shrink-0 rounded-lg">Box 5</div>
+    //   <div className="w-40 h-40 bg-pink-500 shrink-0 rounded-lg">Box 6</div>
+    // </div>
   )
 }
 
