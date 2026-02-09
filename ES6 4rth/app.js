07f16e845e04a614b35209fe6e995d24ca5e4b20
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2026-02-09 23:58:28 +0500

    ok

--- ES6 4rth/app.js
+++ ES6 4rth/app.js
@@ -103,44 +103,107 @@ stdnt1.info()
 stdnt2.info()
 stdnt3.info()
 console.log("Task 3.3")
-class Empslry{
-    constructor(name,age,amount){
-       this.name = name;
-       this.age = age;
-       this.amount = amount;
+class Empslry {
+    constructor(name, age, amount) {
+        this.name = name;
+        this.age = age;
+        this.amount = amount;
     }
-    cns(){
+    cns() {
         console.log(`hy : ${this.name},
             your Age : ${this.age},
             your selry : ${this.amount}`)
     }
 }
-class Mngrselry extends Empslry{
-    constructor(name , age,amount){
-        super(name,age,amount);
-        this.totalslry  = this.amount + 1200
+class Mngrselry extends Empslry {
+    constructor(name, age, amount) {
+        super(name, age, amount);
+        this.totalslry = this.amount + 1200
     }
-    cns(){
+    cns() {
         console.log(`hi ${this.name},
             your age : ${this.age}
             your selry :${this.totalslry}`)
     }
 }
-let umer = new Mngrselry("Umer",19,20000);
-let ali = new Empslry("Ali",21,18000);
+let umer = new Mngrselry("Umer", 19, 20000);
+let ali = new Empslry("Ali", 21, 18000);
 umer.cns();
 ali.cns();
 console.log("Task 4.1");
-function tm(){
-    return new Date().toLocaleDateString()
+function tm() {
+    return new Date().toLocaleTimeString();
 }
 console.log(`1st ${tm()}`);
-function s2(){
+function s2() {
     console.log(`2nd ${tm()}`)
-}   
+}
 s2()
 setTimeout(() => {
     console.log(`3rd ${tm()}`)
 }, 1500);
-console.log( `last ${tm()}`);
-console.log("Task 4.1");
+console.log(`last ${tm()}`);
+console.log("Task 4.2");
+function tim() {
+    return new Date().toLocaleTimeString();
+}
+function stp1(clbk) {
+    setTimeout(() => {
+        console.log(`first ${tim()}`);
+        clbk();
+    }, 1000);
+}
+function stp2(clbk) {
+    setTimeout(() => {
+        console.log(`second ${tim()}`);
+        clbk();
+    }, 1000);
+}
+function stp3(clbk) {
+    setTimeout(() => {
+        console.log(`third ${tim()}`);
+        clbk();
+    }, 1000);
+}
+stp1(() => {
+    stp2(() => {
+        stp3(() => {
+            console.log(`last ${tim()}`);
+        });
+    });
+});
+console.log("Task 4.3");
+
+function tim() {
+    return new Date().toLocaleTimeString();
+}
+function stp1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`first ${tim()}`);
+            resolve();
+        }, 1000);
+    });
+}
+function stp2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`second ${tim()}`);
+            resolve();
+        }, 1000);
+    });
+}
+function stp3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`third ${tim()}`);
+            resolve();
+        }, 1000);
+    });
+}
+stp1()
+    .then(() => stp2())
+    .then(() => stp3())
+    .then(() => {
+        console.log(`last ${tim()}`);
+    });
