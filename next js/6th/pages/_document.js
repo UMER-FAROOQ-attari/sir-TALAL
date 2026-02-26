92119b4c453b9315ba360bf801f9c7ee149b10be
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2026-02-27 02:53:01 +0500

    ofho ok yar

--- next js/6th/pages/_document.js
+++ next js/6th/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Header } from "./Header";
+import  Header  from "./Header";
 export default function Document() {
   return (
     <Html lang="en">
